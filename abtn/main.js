70a1fe0980455a8da3c49d331dab35152d41bad0
oleg prohazko <olegprohazko@outlook.com>
2014-09-21 21:28:57 +0400

    add abtn

---
+++ abtn/main.js
@@ -0,0 +1,102 @@
+var initial = {
+    columns: 3,
+    width  : 120
+}
+
+function Button(category, title) {
+    this.category = category;
+    this.title = title;
+}
+
+Button.prototype.render = function () {
+    var width = this.category.root.width;
+    var cls = this.category.cls;
+    this.$btn = $('<input>').attr('type', 'button')
+        .addClass('btn ' + cls)
+        .width(width)
+        .val(this.title);
+
+    this.category.$buttons.append(this.$btn)
+};
+
+
+function Category(root, title) {
+    this.root = root;
+    this.title = title;
+    this.buttons = [];
+}
+
+Category.prototype.render = function () {
+    this.$header = $('<h1>').text(this.title)
+    this.$btnAdd = $('<input>').attr('type', 'button').css('font-size', 'medium').val('+');
+    var width = this.root.width * this.root.columns + this.root.width * 1.5
+    this.$buttons = $('<div>').width(width)
+
+    this.$btnAdd.click(function () {
+        this.addButton();
+    }.bind(this));
+    this.root.$work.append(this.$header);
+    this.root.$work.append(this.$buttons);
+    this.$header.append(this.$btnAdd);
+
+    this.buttons.forEach(function (btn) {
+        btn.render();
+    }, this)
+};
+
+Category.prototype.addButton = function (name) {
+    if (!name) name = prompt('Category name');
+    if (!name) return;
+
+    var button = new Button(this, name);
+    button.render();
+    this.buttons.push(button);
+}
+
+
+function Main($view) {
+    this.$view = $view;
+    this.$work = $view.find('#work-area')
+    this.categories = []
+    this.columns = initial.columns;   
+    this.width   = initial.width;
+
+    $('#btn-category-add').click(function () {
+        this.addCategory();
+    }.bind(this))
+}
+
+Main.prototype.addCategory = function (name) {
+    if (!name) name = prompt('Category name');
+    if (!name) return;
+
+    var category = new Category(this, name);
+    category.render();
+    this.categories.push(category);
+}
+
+Main.prototype.render = function () {
+    //this.addCategory('test');
+    this.categories.forEach(function (cat) {
+        cat.render();
+    }, this)
+}
+
+function init() {
+
+    var main  = new Main($(document));
+    var store = new ButtonStore(main);
+
+    store.load().then(function () {
+        main.render();
+    })
+
+    console.log('init')
+
+    window['main'] = main;
+}
+
+
+
+$(init);
+
---
+++ abtn/store.js
@@ -0,0 +1,42 @@
+
+
+/*
+  
+  width    : number
+  columns  : number
+  categories   : { 
+     title   : string,
+     buttons : { title : string, cls: string }[] 
+  }[]
+
+*/
+
+
+function ButtonStore(root) {
+    this.root = root;
+}
+
+ButtonStore.prototype.load = function () {
+    return $.getJSON('abtn.json').then(this.onload.bind(this));
+}
+
+ButtonStore.prototype.onload = function (stored) {
+    console.log('stored', stored)
+    this.root.columns = stored.columns || initial.columns;
+    this.root.width   = stored.width   || initial.width;
+    
+    (stored.categories || []).forEach(function (cat) {
+        var category = new Category(this.root, cat.title);
+        category.cls = cat.cls;
+        (cat.buttons || []).forEach(function (btn) {
+            var button = new Button(category, btn.title);
+            category.buttons.push(button);
+        }, this)
+        this.root.categories.push(category)
+    }, this);
+}
+
+ButtonStore.prototype.toJSON = function () {
+    
+}
+
