e717352e59842c92f16e6605e0db078ef3d9bee4
oleg prohazko <olegprohazko@outlook.com>
2014-09-22 22:04:30 +0400

    darken border

--- abtn/main.js
+++ abtn/main.js
@@ -10,6 +10,27 @@ function Button(category, title) {
     this.color = null;
 }
 
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1, 3), 16);
+    var G = parseInt(color.substring(3, 5), 16);
+    var B = parseInt(color.substring(5, 7), 16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R < 255) ? R : 255;
+    G = (G < 255) ? G : 255;
+    B = (B < 255) ? B : 255;
+
+    var RR = ((R.toString(16).length == 1) ? "0" + R.toString(16) : R.toString(16));
+    var GG = ((G.toString(16).length == 1) ? "0" + G.toString(16) : G.toString(16));
+    var BB = ((B.toString(16).length == 1) ? "0" + B.toString(16) : B.toString(16));
+
+    return "#" + RR + GG + BB;
+}
+
 Button.prototype.render = function () {
     var width = this.category.root.width;
     var cls = this.category.cls;
@@ -20,7 +41,7 @@ Button.prototype.render = function () {
         .val(this.title);
     if (this.color != null) {
         this.$btn.css('background-color', this.color)
-        this.$btn.css('border-color', this.color)
+        this.$btn.css('border-color', shadeColor( this.color, -17))
         $("#colors").append( $("<option>").val(this.color) )
     }
 
